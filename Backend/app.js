9be556de04c104d9428740930389c151911be839
Ayush Acharya <acharyaayush555@gmail.com>
2025-12-02 19:32:41 +0545

    db

--- Backend/app.js
+++ Backend/app.js
@@ -19,7 +19,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../Frontend")));
 
-mongoose.connect(process.env.MONGODB_URL)
+mongoose.connect(process.env.MONGODB_URL,{useNewUrlParser: true,
+useUnifiedTopology: true}
+)
 .then(()=>console.log("Connected to MongoDB"))
 .catch((err)=>console.log("Error connecting to MongoDB:", err));
 
